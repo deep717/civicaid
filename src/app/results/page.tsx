e4cdbaca65da30c83d57b90f96da6cd19175f758
Yoga <deep717@gmail.com>
2025-06-20 17:47:40 -0700

    Fix: resolved variable zip

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -4,11 +4,12 @@ import { useSearchParams } from 'next/navigation';
 
 export default function Results() {
   const params = useSearchParams();
+  
   const zip = params.get('zip');
   const income = Number(params.get('income') || 0);
   const householdSize = Number(params.get('householdSize') || 0);
 
-  const eligibility = income < (1500 + 500 * (householdSize - 1)) ? 'You are Eligible' : 'You are not Eligible';
+  const eligibility = income < (1500 + 500 * (householdSize - 1)) ? 'Eligible' : 'not Eligible';
 
   return (
     <div className="p-10 text-center max-w-xl mx-auto">
@@ -16,7 +17,8 @@ export default function Results() {
       <p className="text-lg mb-2">
         Based on your answers:
       </p>
-      <p className="text-2xl font-bold text-blue-600">
+      <p className="text-sm mt-4 text-gray-600">
+		ZIP Code: {zip}
         You are: {eligibility} for basic SNAP in California
       </p>
       <p className="text-sm text-gray-600 mt-4">This is a simplified estimate. For full results, we’ll soon add more programs.</p>
